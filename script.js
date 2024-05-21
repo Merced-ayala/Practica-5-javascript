1bd5f49876c433603fdc8e42ac433362e6a9325f
Merced Ayala <mercy.ayala08@gmail.com>
2024-05-20 19:57:09 -0500

    Creación proyecto Calculadora

--- script.js
+++ script.js
@@ -9,23 +9,23 @@ lineas
 // comenta una linea
 
 console.log("Hola Mundo")
-console.log("aqui podemos escribir cualquier cosa")
-console.log("adios")
-console.log("no hay autocompletado")
-console.log(12345)
-console.log(-1.123)
+// console.log("aqui podemos escribir cualquier cosa")
+// console.log("adios")
+// console.log("no hay autocompletado")
+// console.log(12345)
+// console.log(-1.123)
 
 // Tipos de datos
 
 /* declara una variable */
 let nombre = "Sebastian"
-console.log(nombre)
+// console.log(nombre)
 nombre = "Juan"
-console.log(nombre)
+// console.log(nombre)
 
 /* declara una constante */
 const pi = 3.1416
-console.log(pi)
+// console.log(pi)
 /* pi = 3  no se puede sobreescribir en una constante*/
 
 /* javascript antiguo */
@@ -34,7 +34,7 @@ var apellido = "arias"
 const numero1 = 7
 const numero2 = 9
 const resultado = numero1 + numero2
-console.log("resultado:", resultado, "algo")
+// console.log("resultado:", resultado, "algo")
 
 // -------------- Tipos de datos ------------
 
@@ -42,7 +42,7 @@ console.log("resultado:", resultado, "algo")
 const ciudad = "bogota"
 const pais1 = 'colombia'
 const pais2 = "argentina"
-console.log(ciudad, pais1, pais2)
+// console.log(ciudad, pais1, pais2)
 
 // lueguaje debilmente Tipado
 // Int, float - numeros
@@ -50,8 +50,8 @@ const suma = 5
 const edad = 20.5
 const numeroNegativo = "-10"
 
-console.log("multiplicacion:", 5 * "5")
-console.log(suma, edad, numeroNegativo)
+// console.log("multiplicacion:", 5 * 5)
+// console.log(suma, edad, numeroNegativo)
 
 // Booleandos - Bool - verdaro o falso
 let esVerde = true
@@ -62,12 +62,12 @@ let esMorado = false
 // Array - Vectores - Listas
 
 let listaDeNumeros = [20, -55, 100]
-console.log(listaDeNumeros)
+// console.log(listaDeNumeros)
 
 // ver un elemento de la lista
-console.log(listaDeNumeros[0])
-console.log(listaDeNumeros[1])
-console.log(listaDeNumeros[2])
+// console.log(listaDeNumeros[0])
+// console.log(listaDeNumeros[1])
+// console.log(listaDeNumeros[2])
 
 let listaCiudades = [
     "bogotá",
@@ -75,16 +75,16 @@ let listaCiudades = [
     "cali"
 ]
 
-console.log(listaCiudades)
+// console.log(listaCiudades)
 
 listaCiudades[2] = "boyaca"
-console.log(listaCiudades)
+// console.log(listaCiudades)
 
 let listaDeListas = [listaDeNumeros, listaCiudades]
-console.log(listaDeListas)
+// console.log(listaDeListas)
 
 // acceder al dato "medellin"
-console.log(listaDeListas[1][1])
+// console.log(listaDeListas[1][1])
 
 // JSON (JavaScript Object Notation) - Objetos
 // clave - valor
@@ -99,61 +99,133 @@ let usuario = {
     amigos: ["andres", "pepito"]
 }
 
-console.log(usuario)
-console.log(usuario.nombre)
-console.log(usuario.ubicacion.latitud)
+// console.log(usuario)
+// console.log(usuario.nombre)
+// console.log(usuario.ubicacion.latitud)
 
 usuario.edad = 25
-console.log(usuario)
+// console.log(usuario)
 
-//TIpos de datos vacíos
+// --------- tipos de datos vacios ------
 
-// Null = nulo 
-let espacioVacio = null 
+// null - nulo
+let espacioVacio = null
 
-// undefined =  indefinido
+// undefined - indefinido
 let noDefinido = undefined
-console.log (usuario.apellido)
+// console.log(usuario.apellido)
 let algo
-console.log (algo)
+// console.log(algo)
 
-// NaN - Not a Number - No es un número
-const NoEsNumero = NaN
-const multiplicacion = 10 * 'Nan'
-console.log = (multiplicacion)
-// console.log multiplicación
+// NaN - Not a Number - No es un numero
+const noEsNumero = NaN
+const multiplicacion = 10 * 'hola'
+// console.log(multiplicacion)
 
-//------- Estructuras de control -----
-// Bucles - Un proceso que se repite
-// loop - bucle infinido 
+
+// --------- Estructuras de control -------
+
+// Bucles - un proceso que se repite
+// loop - blucle infinito
 
 const listaAnimales = [
-    "Perro",
+    "perro",
     "gato",
     "oso",
     "gallina",
+    "lobo",
     "zorro"
 ]
 
-//Parámetro 1 = contador
-//Parámetro 2 = condición para que se detenga
-//Parámetro 3 = comportamiento del contador
-//for(let contador = 0; contador = listaAnimales.length; contador = contador +1 )
+// console.log('tamaño del array:', listaAnimales.length)
 
-//for (let i =0; i < listaAnimales.leght; i)
-//console.log(i)
-//
-
-for(let i = 0; i < listaAnimales.length; i++ ) {
-    console.log (i)
-    console.log (listaAnimales[i])
+// parametro 1: contador
+// parametro 2: condicion para que se detenga
+// paramatro 3: comportamiento del contador
+// for(let contador = 0; contador < listaAnimales.length; contador = contador + 1) {
+for (let i = 0; i < listaAnimales.length; i++) {
+    console.log(i)
+    console.log(listaAnimales[i])
 }
 
-//Condiciones 
-//- Igualdad : ===
-//- menor que : <
-//- mayor que:  >
-//- menor o igual a que: <=
-//- mayor o igual a que: >=
+// condiciones
+// - igualdad: ===
+// - diferente de: !==
+// - menor que: <
+// - mayor que: >
+// - menor o igual que: <=
+// - mayor o igual que: >=
+
+let numero = 1
+
+if (numero === 10) {
+    console.log('numero es 10')
+} else if (numero === 11) {
+    console.log('numero es 11')
+} else if (numero === 12) {
+    console.log('numero es 12')
+} else {
+    console.log('no es ni 10 ni 11 ni 12')
+}
+
+// ---------- Operadores logicos ---------
+
+// or - o - ||
+let texto = "c"
+
+if (texto === "a" || texto === "b" || texto === "c") {
+    console.log('la condicion es verdadera')
+} else {
+    console.log("el texto no es ni 'a' ni 'b'")
+}
+
+// and - y - &&
+let numero3 = 20
+
+if (numero3 > 0 && numero3 < 10) {
+    console.log('nuestro numero esta entre el 0 y el 10')
+} else {
+    console.log('nuestro numero no esta entre el 0 y el 10')
+}
+
+// -------- bucles - while - mientras ------
+
+let contador = 0
+while (contador < 5) { // ciclo 3 - contador = 2
+    console.log(contador)
+    console.log(listaAnimales[contador])
+
+    contador++
+}
+
+// -------------- funciones ----------------
+// estructuras reciben parametros, procesarlos
+// y devulven paramtros
+
+// Caracteristicas
+// 1. agrupar codigo relacionado a un cierto fin
+// 2. la funcion es reutilizable
+
+// declaracion de la funcion JavaScript Antiguo
+// function sumar(valor1, valor2) {
+//     let resultado = valor1 + valor2
+//     return resultado
+// }
+
+// declaracion de la funcion con ECMAScript 
+// funcion flecha
+const sumar = (valor1, valor2) => {
+    let resultado = valor1 + valor2
+    return resultado
+}
 
 
+// ejecucion de la funcion \\ llamado de la funcion
+
+let resultadoSuma = sumar(5, 10)
+console.log("resultad de la suma1", resultadoSuma)
+
+console.log("resultad de la suma2", sumar(50, 15))
+console.log("resultad de la suma2", sumar(100, 8))
+console.log("resultad de la suma2", sumar(20, 1))
+console.log("resultad de la suma2", sumar(5, 2))
